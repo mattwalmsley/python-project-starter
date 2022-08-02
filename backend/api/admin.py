a0de0733191d3eec9ac71e5f20262c5d4d0be12f
matt <matthewkjrwalmsley@hotmail.co.uk>
2022-08-02 11:06:53 +0100

    Add Intern and InternAdmin classes.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Intern
+
+# create a class for the admin-model integration
+class InternAdmin(admin.ModelAdmin):
+
+    # add the fields of the model here
+    list_display = (
+        "name",
+        "university",
+        "course",
+        "LoB",
+        "location",
+        "interest1",
+        "interest2",
+        "interest3",
+        "programming 1",
+        "programming 2",
+        "request_time"
+    )
+
+# we will need to register the
+# model class and the Admin model class
+# using the register() method
+# of admin.site class
+admin.site.register(Intern, InternAdmin)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Intern(models.Model):
+    name = models.CharField(max_length=150)
+    university = models.CharField(max_length=150)
+    course = models.CharField(max_length=150)
+    LoB = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
+    interest1 = models.CharField(max_length=150)
+    interest2 = models.CharField(max_length=150)
+    interest3 = models.CharField(max_length=150)
+    programming1 = models.CharField(max_length=150)
+    programming2 = models.CharField(max_length=150)
+    request_time = models.FloatField
+
+    def __str__(self):
+
+        return self.name
