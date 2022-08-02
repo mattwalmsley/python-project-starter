6f92e752585ca6a5f0047fc856f8c07c01b61700
najeebasif <91464925+najeebasif@users.noreply.github.com>
2022-08-02 14:16:06 +0100

    adding changes for buddy apu

--- backend/api/models.py
+++ backend/api/models.py
@@ -17,3 +17,21 @@ class Intern(models.Model):
     def __str__(self):
 
         return self.name
+
+
+class Buddy(models.Model):
+    name = models.CharField(max_length=150)
+    university = models.CharField(max_length=150)
+    course = models.CharField(max_length=150)
+    LoB = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
+    interest1 = models.CharField(max_length=150)
+    interest2 = models.CharField(max_length=150)
+    interest3 = models.CharField(max_length=150)
+    programming1 = models.CharField(max_length=150)
+    programming2 = models.CharField(max_length=150)
+    time_available = models.CharField(max_length=150)
+
+    def __str__(self):
+
+        return self.name
