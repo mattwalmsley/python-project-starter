849592c82ceaf66f9b05311b5ae08bb9949ea5de
najeebasif <91464925+najeebasif@users.noreply.github.com>
2022-08-02 15:30:29 +0100

    adding results api

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,10 @@
+
 # import serializers from the REST framework
 from rest_framework import serializers
 
 # import the todo data model
 from .models import AnalyseMatches
+from .models import ResultModel
 
 # create a serializer class
 class AnalyseMatchesSerializer(serializers.ModelSerializer):
@@ -11,3 +13,10 @@ class AnalyseMatchesSerializer(serializers.ModelSerializer):
     class Meta:
         model = AnalyseMatches
         fields = ("id", "name", "preference1", "preference2", "preference3")
+
+class ResultModelSerializer(serializers.ModelSerializer):
+
+    # create a meta class
+    class Meta:
+        model = ResultModel
+        fields = ("id", "internName", "buddy1", "buddy2", "buddy3")
