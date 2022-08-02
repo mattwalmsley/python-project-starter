33aa06d49e91cbf1f6facd89b335d2f97b2a08a7
najeebasif <91464925+najeebasif@users.noreply.github.com>
2022-08-02 14:17:23 +0100

    adding buddy api

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Intern
+from .models import Buddy
 
 # create a class for the admin-model integration
 class InternAdmin(admin.ModelAdmin):
@@ -21,8 +22,27 @@ class InternAdmin(admin.ModelAdmin):
         "request_time"
     )
 
+
+class BuddyAdmin(admin.ModelAdmin):
+
+    # add the fields of the model here
+    list_display = (
+        "name",
+        "university",
+        "course",
+        "LoB",
+        "location",
+        "interest1",
+        "interest2",
+        "interest3",
+        "programming1",
+        "programming2",
+        "time_available"
+    )
+
 # we will need to register the
 # model class and the Admin model class
 # using the register() method
 # of admin.site class
 admin.site.register(Intern, InternAdmin)
+admin.site.register(Buddy, BuddyAdmin)
